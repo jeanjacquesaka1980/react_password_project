6ba32d2922a053147477b66684ea62d5e9380caf
Jeremy <jeremy.bertin76@hotmail.fr>
2020-01-14 16:21:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const charsArray = ["&", "("];
+  let finalArray = [...numbersArray, ...charsArray];
+
+  let permutation = [];
+  // emptyArray = [...numbersArray, ...charsArray];
+
+  finalArray.map(el => {
+    finalArray.map(el2 => {
+      finalArray.map(el3 => {
+        finalArray.map(el4 => {
+          permutation.push([el, el2, el3, el4])
+        })
+      })
+    })   
+  });
+
+const generatePass = (arr) => {
+  const array = [];
+  for (let i = 0; i < 4; i ++){
+
+    let random = Math.floor(Math.random() * arr.length)
+    //console.log(random)
+    array.push(arr[random]);
+  }
+  return array.join("")
+}
+
+let password = generatePass(finalArray);
+
+let control = permutation.forEach(el => {
+  if (password !== el) {
+    console.log("Not same")
+  }
+})
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {password}
+        {control}
       </header>
     </div>
   );
